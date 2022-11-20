cc703801ba64a04cb04ee3be0c42095273e00f6f
Nicholas Corgan <n.corgan@gmail.com>
2022-11-19 22:39:54 -0600

    Logging

--- plugins/sdr_sources/usrp_sdr_support/main.cpp
+++ plugins/sdr_sources/usrp_sdr_support/main.cpp
@@ -3,13 +3,41 @@
 #include "usrp_sdr_source.h"
 #include "usrp_sdr_sink.h"
 
-#include <SoapySDR/Logger.hpp>
+#include <uhd/utils/log_add.hpp>
 
-// Redirect SoapySDR's logging into SatDump's infrastructure.
-static void satdump_loghandler(const SoapySDR::LogLevel level, const char* message)
+// See: https://github.com/pothosware/SoapyUHD/blob/master/SoapyUHDDevice.cpp
+static void satdump_log(const uhd::log::logging_info &info)
 {
-    (void)level;
-    (void)message;
+    // Build a log message formatted from the information
+    std::string message;
+
+    if (not info.file.empty())
+    {
+        std::string shortfile = info.file.substr(info.file.find_last_of("/\\") + 1);
+        message += "[" + shortfile + ":" + std::to_string(info.line) + "] ";
+    }
+
+    if (not info.component.empty())
+    {
+        message += "[" + info.component + "] ";
+    }
+
+    message += info.message;
+
+    auto spdlog_level = spdlog::level::info;
+
+    switch(info.verbosity)
+    {
+    case uhd::log::trace:   spdlog_level = spdlog::level::trace; break;
+    case uhd::log::debug:   spdlog_level = spdlog::level::debug; break;
+    case uhd::log::info:    spdlog_level = spdlog::level::info; break;
+    case uhd::log::warning: spdlog_level = spdlog::level::warn; break;
+    case uhd::log::error:   spdlog_level = spdlog::level::err; break;
+    case uhd::log::fatal:   spdlog_level = spdlog::level::critical; break;
+    default: break;
+    }
+
+    logger->log(spdlog_level, message);
 }
 
 class UsrpSDRSupport : public satdump::Plugin
@@ -25,7 +53,10 @@ public:
         satdump::eventBus->register_handler<dsp::RegisterDSPSampleSourcesEvent>(registerSources);
         satdump::eventBus->register_handler<dsp::RegisterDSPSampleSinksEvent>(registerSinks);
 
-        SoapySDR::registerLogHandler(satdump_loghandler);
+        // Add SatDump's logging infrastructure as a UHD log handler. Pass all messages through
+        // and let SatDump decide what goes through.
+        uhd::log::add_logger("SatDump", satdump_log);
+        uhd::log::set_log_level(uhd::log::trace);
     }
 
     static void registerSources(const dsp::RegisterDSPSampleSourcesEvent &evt)
